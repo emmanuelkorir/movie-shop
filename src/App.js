4bde173057bc9b54de2350b7f224075c819b1eef
Emmanuel <emmanuelqkorir@gmail.com>
2022-09-07 20:10:14 +0300

    add HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
+import HomeScreen from './Pages/HomeScreen';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container>
+        <HomeScreen />
+      </Container>
       <Footer />
     </div>
   );
