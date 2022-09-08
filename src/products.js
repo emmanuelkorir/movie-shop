6c76cfc854f340373dcc5a9714005454ae7fb11a
Emmanuel <emmanuelqkorir@gmail.com>
2022-09-08 11:20:11 +0300

    update products

---
+++ src/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Enemy Within',
+    image: '/images/enemywithin.jpg',
+    description:
+      'When a Japanese pilot crash-lands on the tiny remote Hawaiian island of Niihau, he is met with courtesy and traditional Hawaiian hospitality from the locals - until they discover he was part of the recent attack on Pearl Harbor.',
+    brand: 'Apple',
+    genre: 'Thriller',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'The Invitation',
+    image: '/images/invitation.jpg',
+    description:
+      'After the death of her mother, Evie is approached by an unknown cousin who invites her to a lavish wedding in the English countryside. Soon, she realizes a gothic conspiracy is afoot and must fight for survival as she uncovers twisted secrets in her familys history.',
+    brand: 'Apple',
+    genre: 'Horror',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/kind.jpg',
+    description:
+      'Follows three brothers who are going to discover the greatest secret of their late mother. This trip is going to change their lives.',
+    brand: 'Cannon',
+    genre: 'Comedy',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'The Pearls',
+    image: '/images/pearls.jpg',
+    description:
+      'During a night of “Adventure Cuisine,” four people find themselves in a gamut of escalating courses, each more surreal than the next. The food seems to be triggering reactions in each of them, bringing out emotions and secrets that would otherwise stay locked up.',
+    brand: 'Sony',
+    genre: 'Horror',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'The Pepper Land',
+    image: '/images/pepperland.jpg',
+    description:
+      'Baran, a Kurdish independence war hero, is now sheriff in Erbil, the capital city. No longer feeling useful in this society now at peace, he thinks about quitting the police force, but instead agrees to be stationed in a small valley, at the very borders of Iran, Turkey, and Iraq.s',
+    brand: 'Logitech',
+    genre: 'Drama',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Spiderman',
+    image: '/images/spiderman.jpg',
+    description:
+      'After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.',
+    brand: 'Amazon',
+    genre: 'Action',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '6',
+    name: 'Top Gun',
+    image: '/images/topgun.jpg',
+    description:
+      'After more than thirty years of service as one of the Navy top aviators, and dodging the advancement in rank that would ground him, Pete “Maverick” Mitchell finds himself training a detachment of TOP GUN graduates for a specialized mission the likes of which no living pilot has ever seen.',
+    brand: 'Amazon',
+    genre: 'Action',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+module.exports = products;
