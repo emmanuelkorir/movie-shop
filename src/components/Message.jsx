7f983ecc4d91f67ed1b2aeb337c5bce7bf12cde5
Emmanuel <emmanuelqkorir@gmail.com>
2022-09-08 11:41:44 +0300

    update message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Alert } from 'react-bootstrap';
 
-function Message() {
-  return <div>Message</div>;
+function Message({ variant, children }) {
+  return (
+    <Alert key={variant} variant={variant}>
+      {children}
+    </Alert>
+  );
 }
+
+Message.defaultProps = {
+  variant: 'info',
+};
 
 export default Message;
